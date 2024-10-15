567c5e808a6f2823d5f560ced2a63a0dc11f89f6
mustafa744 <mustafaashraf744@gmail.com>
2024-10-15 22:34:08 +0300

    auto discovery

--- src/TISControlProtocol/api.py
+++ src/TISControlProtocol/api.py
@@ -1,17 +1,21 @@
-from TISControlProtocol.Protocols import setup_udp_protocol
-from TISControlProtocol.Protocols.udp.ProtocolHandler import (
-    TISProtocolHandler,
-)
-
-from homeassistant.core import HomeAssistant  # type: ignore
+import asyncio
+import json
+import logging
 
 # type: ignore
 import socket
-import logging
 from collections import defaultdict
-import json
-import asyncio
 
+import aiofiles
+from homeassistant.core import HomeAssistant  # type: ignore
+
+from TISControlProtocol.Protocols import setup_udp_protocol
+from TISControlProtocol.Protocols.udp.ProtocolHandler import (
+    TISPacket,
+    TISProtocolHandler,
+)
+
+from .DiscoveryHelpers import DEVICE_APPLIANCES
 
 protocol_handler = TISProtocolHandler()
 
@@ -35,6 +39,8 @@ class TISApi:
         self.hass = hass
         self.config_entries = {}
         self.domain = domain
+        self.devices_dict = devices_dict
+        self.discovery_packet: TISPacket = protocol_handler.generate_discovery_packet()
 
     async def connect(self):
         """Connect to the TIS API."""
@@ -53,51 +59,93 @@ class TISApi:
             raise ConnectionError
 
         self.hass.data[self.domain]["discovered_devices"] = []
+        # scan for devices
+        await self.scan_devices()
 
-    async def parse_device_manager_request(self, data: dict) -> None:
-        """Parse the device manager request."""
-        converted = {
-            appliance: {
-                "device_id": [int(n) for n in details[0]["device_id"].split(",")],
-                "appliance_type": details[0]["appliance_type"]
-                .lower()
-                .replace(" ", "_"),
-                "appliance_class": details[0].get("appliance_class", None),
-                "is_protected": bool(int(details[0]["is_protected"])),
-                "gateway": details[0]["gateway"],
-                "channels": [
-                    {
-                        "channel_number": int(detail["channel_number"]),
-                        "channel_name": detail["channel_name"],
-                    }
-                    for detail in details
-                ],
+    # async def get_entities(self, platform: str = None) -> list:
+    #     """Get the stored entities."""
+    #     try:
+    #         with open("appliance_data.json", "r") as f:
+    #             data = json.load(f)
+    #             await self.parse_device_manager_request(data)
+    #     except FileNotFoundError:
+    #         with open("appliance_data.json", "w") as f:
+    #             pass
+    #     await self.parse_device_manager_request(data)
+    #     entities = self.config_entries.get(platform, [])
+    #     return entities
+
+    async def save_devices(self, devices):
+        # Dump to local file
+        async with aiofiles.open("devices_data.json", "w") as f:
+            await f.write(json.dumps({"devices": devices}, indent=4))
+
+    async def load_devices(self) -> list[dict]:
+        # Load from local file
+        async with aiofiles.open("devices_data.json", "r") as f:
+            devices = json.loads(await f.read())
+        return devices
+
+    async def scan_devices(self, prodcast_attempts=10):
+        """Scan for devices."""
+        # clear the previous discovered devices
+        self.hass.data[self.domain]["discovered_devices"] = []
+        # send dicover packet
+        for _ in range(prodcast_attempts):
+            await self.protocol.sender.broadcast_packet(self.discovery_packet)
+            await asyncio.sleep(1)
+        # fetch the devices
+        devices = [
+            {
+                "device_id": device["device_id"],
+                "device_type_code": device["device_type"],
+                "device_type_name": self.devices_dict.get(
+                    tuple(device["device_type"]), tuple(device["device_type"])
+                ),
+                "gateway": ",".join(device["source_ip"]),
             }
-            for appliance, details in data["appliances"].items()
-        }
+            for device in self.hass.data[self.domain]["discovered_devices"]
+        ]
+        # dump to local file
+        await self.save_devices(devices)
 
-        grouped = defaultdict(list)
-        for appliance, details in converted.items():
-            grouped[details["appliance_type"]].append({appliance: details})
+    async def get_entities(self, platform: str):
+        # load devices
+        devices = await self.load_devices()
+        # parse devices
+        appliances = self.parse_saved_devices(devices["devices"])
+        logging.error(
+            "appliances for platform %s: %s", platform, appliances.get(platform, [])
+        )
+        # return appliances
+        return appliances.get(platform, [])
 
-        self.config_entries = dict(grouped)
-        # add a lock module config entry
-        self.config_entries["lock_module"] = {
-            "password": data["configs"]["lock_module_password"]
-        }
-        # return response
-        return self.config_entries
-        # await self.update_entities()
-
-    async def get_entities(self, platform: str = None) -> list:
-        """Get the stored entities."""
-        try:
-            with open("appliance_data.json", "r") as f:
-                data = json.load(f)
-                await self.parse_device_manager_request(data)
-        except FileNotFoundError:
-            with open("appliance_data.json", "w") as f:
-                pass
-        await self.parse_device_manager_request(data)
-        entities = self.config_entries.get(platform, [])
-        return entities
+    def parse_saved_devices(self, devices: list[dict]):
+        """convert saved devices payload to usable format"""
+        appliances = {}
+        for device in devices:
+            # get the device appliances
+            device_appliances = DEVICE_APPLIANCES.get(
+                tuple(device["device_type_code"]), None
+            )
+            if device_appliances:
+                # itterate over the appliances
+                for platform, count in device_appliances["appliances"].items():
+                    # check if platform exists if not create key with empty list
+                    if platform not in appliances:
+                        appliances[platform] = []
+                    for i in range(count):
+                        appliance = {
+                            "name": f"{str(device['device_id'])} {platform} channel{i}",
+                            "device_id": device["device_id"],
+                            "device_type_name": device["device_type_name"],
+                            "gateway": device["gateway"],
+                            "channels": [
+                                {
+                                    "Output": i,
+                                }
+                            ],
+                            "is_protected": False,
+                        }
+                        appliances[platform].append(appliance)
+        return appliances
