750ee775a6a0a38fad1cddfbf48ab742fde8a958
ellenhomeassistant <ellenhomeassistant@gmail.com>
2025-05-24 00:06:05 +0300

    edit energy event

--- src/TISControlProtocol/Protocols/udp/PacketHandlers/EnergyFeedbackHandler.py
+++ src/TISControlProtocol/Protocols/udp/PacketHandlers/EnergyFeedbackHandler.py
@@ -12,7 +12,8 @@ async def handle_energy_feedback(hass: HomeAssistant, info: dict):
 
     if sub_operation == 0xDA:
 
-        energy = int((info["additional_bytes"][12]<<8)|(info["additional_bytes"][13]))
+        energy = [int((info["additional_bytes"][12]<<8)|(info["additional_bytes"][13])),
+                  int((info["additional_bytes"][6]<<8)|(info["additional_bytes"][7]))]
 
         event_data = {
             "device_id": device_id,
