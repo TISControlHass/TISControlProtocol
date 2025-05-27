e0fc5038763ebba7657a21cf437a20d9adb119dc
Ibrahim Mohamed <106034477+IbrahimMohamed2001@users.noreply.github.com>
2025-05-27 16:05:00 +0300

    refactor: enhance energy feedback handling for monthly energy data

--- src/TISControlProtocol/Protocols/udp/PacketHandlers/EnergyFeedbackHandler.py
+++ src/TISControlProtocol/Protocols/udp/PacketHandlers/EnergyFeedbackHandler.py
@@ -17,7 +17,25 @@ async def handle_energy_feedback(hass: HomeAssistant, info: dict):
     channel_num = int(info["additional_bytes"][0]) + 1
     sub_operation = int(info["additional_bytes"][1])
 
-    if sub_operation == 0x65:
+    if sub_operation == 0xDA:
+        # [energy, monthly power, phase1, phase2, phase3]
+        energy = int(
+            (info["additional_bytes"][16] << 8) | (info["additional_bytes"][17])
+        )
+
+        event_data = {
+            "device_id": device_id,
+            "channel_num": channel_num,
+            "feedback_type": "monthly_energy_feedback",
+            "energy": energy,
+            "additional_bytes": info["additional_bytes"],
+        }
+
+        try:
+            hass.bus.async_fire(str(info["device_id"]), event_data)
+        except Exception as e:
+            logging.error(f"error in firing event for feedback: {e}")
+    elif sub_operation == 0x65:
         energy = {
             "v1": big_endian_to_num(info["additional_bytes"][3:7]),
             "v2": big_endian_to_num(info["additional_bytes"][7:11]),
