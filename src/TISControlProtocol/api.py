168c4325a4b2a95826b4af47b0181f444da38d9a
Ibrahim Mohamed <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-06-18 17:42:07 +0300

    registering the new endpoint

--- src/TISControlProtocol/api.py
+++ src/TISControlProtocol/api.py
@@ -96,6 +96,7 @@ class TISApi:
             self.hass.http.register_view(ChangeSecurityPassEndpoint(self))
             self.hass.http.register_view(RestartEndpoint(self))
             self.hass.http.register_view(RestartStatusEndpoint(self))
+            self.hass.http.register_view(UpdateEndpoint(self))
         except Exception as e:
             logging.error("Error registering views %s", e)
             raise ConnectionError
