d3123c7ae6290bded428202b0802893a7637b181
Tis-Control-HA <tishomeassistant@gmail.com>
2025-08-20 14:20:27 +0300

    update health sensor

--- src/TISControlProtocol/Protocols/udp/PacketHandlers/HealthFeedbackHandler.py
+++ src/TISControlProtocol/Protocols/udp/PacketHandlers/HealthFeedbackHandler.py
@@ -6,17 +6,25 @@ async def handle_health_feedback(hass: HomeAssistant, info: dict):
     """
     Handle the feedback from a health sensor.
     """
-    co = 0
+
     device_id = info["device_id"]
-    lux = int((info["additional_bytes"][5]<<8)|(info["additional_bytes"][6]))
-    noise = int((info["additional_bytes"][7]<<8)|(info["additional_bytes"][8]))
-    eco2 = int((info["additional_bytes"][9]<<8)|(info["additional_bytes"][10]))
-    tvoc = int((info["additional_bytes"][11]<<8)|(info["additional_bytes"][12]))
-    temp = int(info["additional_bytes"][15])
+
+    # this try to know if this device is 10F or health sensor, for 10F it will make all readings 0 except LUX
     try:
-        co = int((info["additional_bytes"][27]<<8)|(info["additional_bytes"][28]))
-    except:
-        pass
+        lux = int((info["additional_bytes"][5] << 8) | (info["additional_bytes"][6]))
+        noise = int((info["additional_bytes"][7] << 8) | (info["additional_bytes"][8]))
+        eco2 = int((info["additional_bytes"][9] << 8) | (info["additional_bytes"][10]))
+        tvoc = int((info["additional_bytes"][11] << 8) | (info["additional_bytes"][12]))
+        temp = int(info["additional_bytes"][13])
+        humidity = int(info["additional_bytes"][14])
+        co = int((info["additional_bytes"][27] << 8) | (info["additional_bytes"][28]))
+    except Exception as _:
+        tvoc = 0
+        noise = 0
+        temp = 0
+        humidity = 0
+        eco2 = 0
+        co = 0
 
     event_data = {
         "device_id": device_id,
@@ -27,9 +35,10 @@ async def handle_health_feedback(hass: HomeAssistant, info: dict):
         "tvoc": tvoc,
         "co": co,
         "temp": temp,
+        "humidity": humidity,
         "additional_bytes": info["additional_bytes"],
     }
-    
+
     try:
         hass.bus.async_fire(str(info["device_id"]), event_data)
     except Exception as e:
