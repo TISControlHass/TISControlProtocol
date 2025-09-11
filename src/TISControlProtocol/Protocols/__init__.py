127e348efdc9a0d61de7c703485c79fb8f1cda61
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-09-11 16:06:26 +0300

    adding types to increase the clarity of the code

--- src/TISControlProtocol/Protocols/__init__.py
+++ src/TISControlProtocol/Protocols/__init__.py
@@ -1,12 +1,17 @@
 from asyncio import get_event_loop, AbstractEventLoop
 import socket
 from TISControlProtocol.Protocols.udp.PacketProtocol import PacketProtocol
+from homeassistant.core import HomeAssistant
 
 loop = get_event_loop()
 
 
 async def setup_udp_protocol(
-    sock: socket, loop: AbstractEventLoop, udp_ip, udp_port, hass
+    sock: socket,
+    loop: AbstractEventLoop,
+    udp_ip: str,
+    udp_port: int,
+    hass: HomeAssistant,
 ) -> tuple[socket.socket, PacketProtocol]:
     transport, protocol = await loop.create_datagram_endpoint(
         lambda: PacketProtocol(sock, udp_ip, udp_port, hass),
