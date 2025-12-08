63dec2a63b973264d356878ce45ecabcbffb467a
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-12-08 16:50:32 +0200

    adding the endpoint to fire the password

---
+++ src/TISControlProtocol/apis/submit_password.py
@@ -0,0 +1,35 @@
+from homeassistant.components.http import HomeAssistantView
+from aiohttp import web
+import logging
+
+
+class SubmitPasswordEndpoint(HomeAssistantView):
+    """TIS API endpoint."""
+
+    url = "/api/submit-password"
+    name = "api:submit-password"
+    requires_auth = False
+
+    def __init__(self, tis_api):
+        self.tis_api = tis_api
+
+    async def post(self, request):
+        try:
+            data = await request.json()
+
+            if not data or "password" not in data:
+                logging.error("Required parameters are missing in the request")
+                return web.json_response(
+                    {"error": "Required parameters are missing"}, status=400
+                )
+
+            password = data["password"]
+            self.tis_api.hass.bus.async_fire("password_feedback", password)
+
+            # Return the response immediately
+            return web.json_response({"message": "Password submitted successfully"})
+        except Exception as e:
+            logging.error(f"Error submitting password: {e}")
+            return web.json_response(
+                {"error": "Failed to submit the password"}, status=500
+            )
