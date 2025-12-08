e4a18dc8827560add2222f0c9cee1f9dd76b5d05
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-12-08 17:58:06 +0200

    adding endpoint to show the password dashboard

---
+++ src/TISControlProtocol/apis/password_dashboard.py
@@ -0,0 +1,18 @@
+from homeassistant.components.http import HomeAssistantView
+import os
+from aiohttp import web
+
+
+class PasswordDashboardEndpoint(HomeAssistantView):
+    """TIS API endpoint."""
+
+    url = "/api/password-dashboard"
+    name = "api:password-dashboard"
+    requires_auth = False
+
+    def __init__(self, views_path):
+        self.views_path = views_path
+
+    async def get(self, request):
+        file_path = os.path.join(self.views_path, "password_dashboard", "index.html")
+        return web.FileResponse(file_path)
