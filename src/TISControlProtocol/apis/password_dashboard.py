bb0afaf3b1a03edb2f002439137254ba034b889d
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-12-10 16:58:56 +0200

    returning back authentication requirement

--- src/TISControlProtocol/apis/password_dashboard.py
+++ src/TISControlProtocol/apis/password_dashboard.py
@@ -11,12 +11,16 @@ class PasswordDashboardEndpoint(HomeAssistantView):
 
     url = "/api/password-dashboard"
     name = "api:password-dashboard"
-    requires_auth = False
 
     def __init__(self, views_path):
         self.views_path = views_path
 
     async def get(self, request):
+        user = request["hass_user"]
+
+        if not user.is_admin:
+            return web.Response(status=403, text="Admins Only")
+
         file_path = os.path.join(self.views_path, "password_dashboard", "index.html")
         if not os.path.exists(file_path):
             return web.Response(text="Error: Dashboard file not found", status=404)
