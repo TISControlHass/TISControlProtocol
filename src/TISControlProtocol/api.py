c0b5d37ce8569537e47ea0ef117fe2012003f98b
IbrahimMohamed2001 <Ibrahim.Hassan01@eng-st.cu.edu.eg>
2026-07-26 12:50:10 +0300

    clean up everything and use the manager module

--- src/TISControlProtocol/api.py
+++ src/TISControlProtocol/api.py
@@ -3,18 +3,14 @@ import logging
 import os
 import socket
 from collections import defaultdict
-from datetime import timedelta
 from typing import Optional
 
 import aiofiles
-import aiohttp
-import psutil
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers.event import async_track_time_interval
 
 from TISControlProtocol.Protocols import setup_udp_protocol
-from TISControlProtocol.shared import get_real_mac
+
+from .services import CMSService, ServiceManager
 
 from .apis import (
     BillConfigEndpoint,
@@ -61,7 +57,8 @@ class TISApi:
         self.display = None
         self.version = version
         self.cms_url = "https://cms-tis.com"
-        self._cms_task_unsub = None
+        self.service_manager = ServiceManager(self.hass, self.domain)
+        self.service_manager.register(CMSService(self.hass, self.domain, self.cms_url))
 
     async def setup(self):
         """Setup the TIS API."""
@@ -134,99 +131,6 @@ class TISApi:
             logging.error("Error registering views %s", e)
             raise ConnectionError
 
-    def _register_services(self):
-        """Register Home Assistant services."""
-        if self.hass.services.has_service(self.domain, "send_cms_data"):
-            return
-
-        logging.info("Registering CMS data service")
-
-        async def handle_cms_data(call):
-            data = call.data.get("data", None)
-
-            if data is None:
-                logging.error("No data provided to send to CMS")
-                return
-
-            cms_sender = CMSDataSender(
-                external_url=f"{self.cms_url}/api/device-health",
-                hass=self.hass,
-            )
-            await cms_sender.send_data(data)
-
-        self.hass.services.async_register(
-            self.domain,
-            "send_cms_data",
-            handle_cms_data,
-        )
-
-    def _schedule_cms_data_task(self):
-        """Schedule periodic CMS data task."""
-
-        async def scheduled_task(now=None):
-            try:
-                data = await self._collect_system_data()
-
-                await self.hass.services.async_call(
-                    self.domain,
-                    "send_cms_data",
-                    {"data": data},
-                )
-            except Exception as e:
-                logging.error(f"Error getting data for CMS: {e}")
-
-        if getattr(self, "_cms_task_unsub", None):
-            self._cms_task_unsub()
-
-        interval = timedelta(minutes=3)
-        self._cms_task_unsub = async_track_time_interval(
-            self.hass, scheduled_task, interval
-        )
-
-    def _unregister_cms_services(self):
-        """Unregister CMS service and stop periodic task."""
-        if self.hass.services.has_service(self.domain, "send_cms_data"):
-            self.hass.services.async_remove(self.domain, "send_cms_data")
-
-        logging.info("CMS data service unregistered")
-
-        if getattr(self, "_cms_task_unsub", None):
-            self._cms_task_unsub()
-            self._cms_task_unsub = None
-
-    async def _collect_system_data(self):
-        """Collect system data for CMS."""
-        # Mac Address
-        mac_address = await get_real_mac("end0")
-
-        # CPU Usage
-        cpu_usage = await self.hass.async_add_executor_job(psutil.cpu_percent, 1)
-
-        # CPU Temperature
-        cpu_temp = await self.hass.async_add_executor_job(psutil.sensors_temperatures)
-        cpu_temp = cpu_temp.get("cpu_thermal", None)
-        cpu_temp = cpu_temp[0].current if cpu_temp else 0
-
-        # Disk Usage
-        total, _, free, percent = await self.hass.async_add_executor_job(
-            psutil.disk_usage, "/"
-        )
-
-        # Memory Usage
-        mem = await self.hass.async_add_executor_job(psutil.virtual_memory)
-
-        return {
-            "mac_address": mac_address,
-            "cpu_usage": cpu_usage,
-            "cpu_temperature": cpu_temp,
-            "disk_total": total,
-            "disk_free": free,
-            "disk_percent": percent,
-            "ram_total": mem.total,
-            "ram_free": mem.free,
-            "ram_percent": mem.percent,
-        }
-
     def run_display(self, style="dots"):
         from .display import TISDisplay
 
@@ -272,11 +176,7 @@ class TISApi:
 
         self.config_entries["passwords"] = data.get("passwords", {})
         self.config_entries["cms"] = bool(data["configs"].get("cms_send_data", False))
-        if self.config_entries["cms"]:
-            self._register_services()
-            self._schedule_cms_data_task()
-        else:
-            self._unregister_cms_services()
+        self.service_manager.toggle("cms", self.config_entries["cms"])
 
         self.config_entries["connected"] = bool(data["configs"].get("connected", True))
         if not self.config_entries["connected"]:
@@ -393,30 +293,3 @@ class TISApi:
             return self.decrypt(data, shift)
         else:
             return data
-
-
-class CMSDataSender:
-    """CMS Data class."""
-
-    def __init__(self, external_url: str, hass: HomeAssistant) -> None:
-        self.external_url = external_url
-        self.hass = hass
-
-    async def send_data(self, data):
-        if data is not None:
-            try:
-                session = async_get_clientsession(self.hass)
-                async with session.post(self.external_url, json=data) as response:
-                    if response.status != 200:
-                        error_text = await response.text()
-                        logging.warning(f"Error sending data to CMS: {response.status}")
-                        logging.info(f"Error response: {error_text}")
-                        return False
-                    else:
-                        logging.info("Data sent to CMS successfully")
-                        return True
-            except aiohttp.ClientError as e:
-                logging.warning(f"ClientError while sending data to CMS: {e}")
-                return False
-
-        return False
