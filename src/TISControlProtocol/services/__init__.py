06a91d113a74fbc2029304590387a46e742d4978
IbrahimMohamed2001 <Ibrahim.Hassan01@eng-st.cu.edu.eg>
2026-07-26 14:31:57 +0300

    add stuff to the init file

--- src/TISControlProtocol/services/__init__.py
+++ src/TISControlProtocol/services/__init__.py
@@ -1,5 +1,12 @@
 from .base import BaseService
 from .cms import CMSDataSender, CMSService
+from .device_scan import DeviceScanService
 from .manager import ServiceManager
 
-__all__ = ["BaseService", "ServiceManager", "CMSService", "CMSDataSender"]
+__all__ = [
+    "BaseService",
+    "ServiceManager",
+    "CMSService",
+    "CMSDataSender",
+    "DeviceScanService",
+]
